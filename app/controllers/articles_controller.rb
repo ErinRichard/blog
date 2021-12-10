608109ab89bbebee288e5d4338eee68c0c437e19
Erin Richard <richard.erind@gmail.com>
2021-12-10 00:32:05 -0600

    add ability to show a single article with new id params in routes.rb, show controller action, and new show.html.erb view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,10 @@ class ArticlesController < ApplicationController
     # Fetch all articles from the database
     @articles = Article.all
   end
+
+  # show action calls Article.find with the ID captured by the route parameter
+  # The returned article is stored in the @article instance variable, so it's accessible by the view
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+
+  # :id designates a route parameter
+  get "/articles/:id", to: "articles#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
