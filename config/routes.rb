6354483f05a15f27b04d9752bda4704e09911dcf
Erin Richard <richard.erind@gmail.com>
2021-12-10 00:51:46 -0600

    added resources method in routes.rb for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # map the root path of the application to the appropriate controller and action
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  # resources is a routes method that maps routes for a collection of resources
+  resources :articles
+end
+
+  # using the resources method for the collection of articles instead of individual get routes
+  # get "/articles", to: "articles#index"
 
   # :id designates a route parameter
-  get "/articles/:id", to: "articles#show"
+  # get "/articles/:id", to: "articles#show"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
